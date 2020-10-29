746495f1f41e1c4ad3c57f53d19a4abe90677fce
detect <detect@detect.localdomain>
2020-10-29 11:17:40 +0200

    rebuild imports

--- extended_shell/__init__.py
+++ extended_shell/__init__.py
@@ -1,1 +1,25 @@
+from importlib import import_module
+
 default_app_config = 'extended_shell.apps.ExtendedShellConfig'
+
+
+def extra_import(pathes):
+    values = {}
+
+    for path in pathes:
+        module_path = key = path
+
+        try:
+            module_path, key = path.rsplit('.', 1)
+        except ValueError:
+            pass
+
+        try:
+            module = import_module(module_path)
+        except ModuleNotFoundError:
+            module = import_module(path)
+
+        values[key] = getattr(
+            module, key, module)
+
+    return values
--- extended_shell/startup.py
+++ extended_shell/startup.py
@@ -1,5 +1,3 @@
-from importlib import import_module
-
 from django.apps import apps
 from django.conf import settings  # noqa
 from django.core.cache import cache  # noqa
@@ -7,26 +5,19 @@ from django.db.models import Avg, Count, F, Max, Min, Q, Sum  # noqa
 from django.urls import reverse  # noqa
 from django.utils import timezone  # noqa
 
-extra_imports = getattr(
-    settings, 'EXTENDED_SHELL_IMPORTS', []
+from extended_shell import extra_import
+
+locals().update(
+    extra_import(getattr(
+        settings, 'EXTENDED_SHELL_IMPORTS', []
+    ))
 )
 
-for path in extra_imports:
-    module_path = key = path
-
-    try:
-        module_path, key = path.rsplit('.', 1)
-    except ValueError:
-        pass
-
-    try:
-        module = import_module(module_path)
-    except ModuleNotFoundError:
-        module = import_module(path)
-
-    locals()[key] = getattr(
-        module, key, module)
+locals().update({
+    model.__name__: model for
+    model in apps.get_models()
+})
 
 
-for model in apps.get_models():
-    locals()[model.__name__] = model
+del extra_import
+del apps
